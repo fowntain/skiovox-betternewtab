a3e6c84dfa1d369185a784bd0f8fd0dcc4a9965b
MunyDev <jeffplays1292@gmail.com>
2023-11-16 18:53:14 -0500

    skiovox_cool: format code

--- background/commands.js
+++ background/commands.js
@@ -1,11 +1,11 @@
 function getRecent(callback) {
   chrome.windows.getLastFocused({ populate: true }, (window) => {
-    callback({window, tabs: window.tabs})
+    callback({ window, tabs: window.tabs })
   });
 }
 
 function cycleTabs(direction) {
-  getRecent(({tabs}) => {
+  getRecent(({ tabs }) => {
     let currentTab = tabs.find((e) => e.active);
     if (!currentTab) return;
 
@@ -18,7 +18,7 @@ function cycleTabs(direction) {
 function onCommand(name, tab) {
   switch (name) {
     case "NEW_TAB":
-      getRecent(({window}) => {
+      getRecent(({ window }) => {
         chrome.tabs.create({ windowId: window?.id });
 
         if (window && window.state === chrome.windows.WindowState.FULLSCREEN) {
@@ -46,7 +46,7 @@ function onCommand(name, tab) {
       break;
 
     case "CLOSE_WINDOW":
-      getRecent(({window}) => {
+      getRecent(({ window }) => {
         if (window.focused) {
           chrome.windows.remove(window.id);
         }
@@ -64,7 +64,7 @@ function onCommand(name, tab) {
     case "SWITCH_WINDOWS":
       chrome.windows.getAll((windows) => {
         if (windows.length === 1) return;
-        getRecent(({window}) => {
+        getRecent(({ window }) => {
           chrome.windows.update(window.id, { focused: false });
         });
       })
@@ -79,7 +79,7 @@ function onCommand(name, tab) {
     case "CTRL_7":
     case "CTRL_8":
       let num = Number(name.split("_")[1]);
-      getRecent(({tabs}) => {
+      getRecent(({ tabs }) => {
         let specifiedTab = tabs[num - 1];
         if (!specifiedTab) return;
 
@@ -88,7 +88,7 @@ function onCommand(name, tab) {
       break;
 
     case "CTRL_9":
-      getRecent(({tabs}) => {
+      getRecent(({ tabs }) => {
         let lastTab = tabs[tabs.length - 1];
         chrome.tabs.update(lastTab.id, { active: true });
       });
@@ -99,44 +99,44 @@ function onCommand(name, tab) {
 chrome.commands.onCommand.addListener(onCommand);
 chrome.runtime.onMessage.addListener(async function (msg, sender, sendResponse) {
   if (msg.type === "loadinspectargets") {
-    await chrome.debugger.attach({targetId: "browser"}, '1.3');
-    var allTargets = await chrome.debugger.sendCommand({targetId: 'browser'}, 'Target.getTargets');
-    await chrome.debugger.detach({targetId: 'browser'});
+    await chrome.debugger.attach({ targetId: "browser" }, '1.3');
+    var allTargets = await chrome.debugger.sendCommand({ targetId: 'browser' }, 'Target.getTargets');
+    await chrome.debugger.detach({ targetId: 'browser' });
     sendResponse({
       data: allTargets.targetInfos,
       type: 'response'
     });
   }
   if (msg.type === "startinspect") {
-    await chrome.debugger.attach({targetId: 'browser'}, '1.3')
-function chromeTabsScriptFull() {
-   var url = URL.createObjectURL(new Blob(["<script>alert(1)</script>"], {type: 'text/html'}));
-   chrome.tabs.create({url: url});
-}
-var id = setInterval(async ()=>{
-
-(await chrome.debugger.sendCommand({targetId: 'browser'}, "Target.getTargets")).targetInfos.forEach(async function (t) {
-    console.log(t.type);
-    if (t.url.includes('chrome-extension://' + msg.extid) && t.type === "service_worker") {
-        var targetId = t.targetId;
-        var {sessionId} = await chrome.debugger.sendCommand({targetId: 'browser'}, 'Target.attachToTarget', {targetId, flatten: false})
-        console.log(await chrome.debugger.sendCommand({targetId: "browser"}, 'Target.sendMessageToTarget', {sessionId, message: JSON.stringify({id: 999, method: "Runtime.evaluate", params: {expression: `(${chromeTabsScriptFull.toString()})()`}})}));
-            chrome.debugger.detach({targetId: 'browser'})
-            sendResponse({
-    "error": null,
-    "data": ""
-  })
-        clearInterval(id)
+    await chrome.debugger.attach({ targetId: 'browser' }, '1.3')
+    function chromeTabsScriptFull() {
+      var url = URL.createObjectURL(new Blob(["<script>alert(1)</script>"], { type: 'text/html' }));
+      chrome.tabs.create({ url: url });
     }
-})
+    var id = setInterval(async () => {
 
-},200);
+      (await chrome.debugger.sendCommand({ targetId: 'browser' }, "Target.getTargets")).targetInfos.forEach(async function (t) {
+        console.log(t.type);
+        if (t.url.includes('chrome-extension://' + msg.extid) && t.type === "service_worker") {
+          var targetId = t.targetId;
+          var { sessionId } = await chrome.debugger.sendCommand({ targetId: 'browser' }, 'Target.attachToTarget', { targetId, flatten: false })
+          console.log(await chrome.debugger.sendCommand({ targetId: "browser" }, 'Target.sendMessageToTarget', { sessionId, message: JSON.stringify({ id: 999, method: "Runtime.evaluate", params: { expression: `(${chromeTabsScriptFull.toString()})()` } }) }));
+          chrome.debugger.detach({ targetId: 'browser' })
+          sendResponse({
+            "error": null,
+            "data": ""
+          })
+          clearInterval(id)
+        }
+      })
 
-  
+    }, 200);
+
+
 
   }
   if (msg.type === "cancelinspect") {
-    await chrome.debugger.detach({targetId: 'browser'});
-    sendResponse({error: null, type: 'response', data: {}});
+    await chrome.debugger.detach({ targetId: 'browser' });
+    sendResponse({ error: null, type: 'response', data: {} });
   }
 })
